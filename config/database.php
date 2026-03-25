b8e3c79845fa98e4d8be28fbfcdb7ec0014a30bc
Amani Koros <amanikalyak@gmail.com>
2026-03-25 13:09:14 +0300

    Fix for TiDB SSL using constant Ids

--- config/database.php
+++ config/database.php
@@ -58,9 +58,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                    // This is the same as PDO::MYSQL_ATTR_SSL_STRATEGY => 'REQUIRED'
+                    1014 => 'REQUIRED', 
+                    // This is the same as PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+                    1013 => false,
+                    ],
         ],
 
         'mariadb' => [
