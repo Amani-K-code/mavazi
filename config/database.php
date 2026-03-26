1a69fa93381bcb1b0fefe4f0ae364f1660455704
Amani Koros <amanikalyak@gmail.com>
2026-03-26 20:25:06 +0300

    database.php - fix

--- config/database.php
+++ config/database.php
@@ -58,10 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [
-                1014 => 'REQUIRED', // PDO::MYSQL_ATTR_SSL_STRATEGY
-                1013 => false,      // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT
-            ],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]) : [],
         ],
 
         'mariadb' => [
